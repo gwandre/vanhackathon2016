2e5e0072c56702cbdd4daa6e1056fd2bf26ede21
Oziel Guimaraes <microzapple@gmail.com>
2016-10-23 16:56:40 -0400

    added game score

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function setProgressbar() {
  * Set window background position by the time elapsed (DAY / NIGHT)
  */
 function setWindow() {
-    var percentDone = (timeElapsed * 100) / progressMax;
+    var percentDone = (timeElapsed * 50) / progressMax;
     var halfSize = ($("#window #img").height() / 2);
     var sunPosition = ((halfSize - (halfSize * percentDone / 100)) * -1) + 2;
     $("#window #img").css("top", sunPosition + "px");
@@ -195,6 +195,7 @@ function gameOver() {
     stopGame();
     firedScreen = true;
     $("#fired").fadeIn();
+    $("#points").html("POINTS: " + totalPoints).fadeIn();
 }
 
 /*
@@ -224,18 +225,22 @@ function checkKey(e) {
     }
     else {
         if (event.keyCode == '38' || event.keyCode == '87') {
+            totalPoints += 100;
             // up arrow
             arrowClick("up");
         }
         else if (event.keyCode == '40' || event.keyCode == '83') {
+            totalPoints += 100;
             // down arrow
             arrowClick("down");
         }
         else if (event.keyCode == '37' || event.keyCode == '65') {
+            totalPoints += 100;
             // left arrow
             arrowClick("left");
         }
         else if (event.keyCode == '39' || event.keyCode == '68') {
+            totalPoints += 100;
             // right arrow
             arrowClick("right");
         }
